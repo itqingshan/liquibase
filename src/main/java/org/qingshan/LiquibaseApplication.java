f61e69382f054fb918ca8f831c6703add743dfef
qingshan <18782870626@163.com>
2023-02-13 14:19:24 +0800

    init(not success.)

---
+++ src/main/java/org/qingshan/LiquibaseApplication.java
@@ -0,0 +1,16 @@
+package org.qingshan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * springboot 集成liquibase
+ */
+@SpringBootApplication
+public class LiquibaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiquibaseApplication.class, args);
+    }
+}
---
+++ src/main/java/org/qingshan/domain/User.java
@@ -0,0 +1,21 @@
+package org.qingshan.domain;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private Integer age;
+
+    private String gender;
+}
